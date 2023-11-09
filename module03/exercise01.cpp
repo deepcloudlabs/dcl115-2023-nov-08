4a4c61813ffb87e55577ab9763eef81d1c7a5228
Binnur KURT <deepcloudlabs@gmail.com>
2023-11-09 13:43:05 +0300

    initial commit

--- module03/exercise01.cpp
+++ module03/exercise01.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <thread>
+
+using namespace std;
+
+static int state = 0;
+mutex mut;
+
+void race() {
+    cerr << "Thread ("
+         << this_thread::get_id()
+         <<") is running..." << endl;
+    for (int i = 0; i < 1'000'000; ++i){
+        { // critical section -> mutex
+            lock_guard<mutex> guard(mut);
+            ++state; // Divisible -> Atomic
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    thread t1(race);
+    thread t2(race);
+    thread t3(race);
+    thread t4(race);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    std::cerr << "State: " << state << std::endl;
     return 0;
 }
---
+++ module03/exercise02.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+class vehicle { // ts
+    const double capacity;
+    double currentLoad;
+
+public:
+    mutex m;
+    explicit vehicle(const double capacity) : capacity(capacity) {
+        this->currentLoad = 0.0;
+    }
+
+    const double getCapacity() const {
+        return capacity;
+    }
+
+    double getCurrentLoad() {
+        lock_guard<mutex> guard(m);
+        return currentLoad;
+    }
+
+    double load(double weight) {
+        lock_guard<mutex> guard(m);
+        if (weight <= 0) return this->currentLoad;
+        if ((weight + this->currentLoad > this->capacity))
+            return this->currentLoad;
+        this->currentLoad += weight;
+        return this->currentLoad;
+    }
+
+    double unload(double weight) {
+        lock_guard<mutex> guard(m);
+        if (weight <= 0) return this->currentLoad;
+        if (weight > this->currentLoad)
+            return this->currentLoad;
+        this->currentLoad -= weight;
+        return this->currentLoad;
+    }
+};
+
+void transferLoad(vehicle& from,vehicle& to,double weight){
+    unique_lock lock1(from.m,defer_lock);
+    unique_lock lock2(to.m,defer_lock);
+    lock(lock1,lock2);
+    from.unload(weight);
+    to.load(weight);
+}
+
+
+void task(vehicle& vehicle1,vehicle& vehicle2){
+    for (int i=0;i<10;++i){
+        vehicle1.load(200);
+        vehicle2.load(200);
+    }
+    for (int i=0;i<10;++i) {
+        transferLoad(vehicle1, vehicle2, 50);
+    }
+}
+int main() {
+    vehicle v1(10'000);
+    vehicle v2(5'000);
+    thread t1(task,ref(v1),ref(v2));
+    thread t2(task,ref(v1),ref(v2));
+    t1.join();
+    t2.join();
+    std::cerr << "State: " << state << std::endl;
+    return 0;
+}
